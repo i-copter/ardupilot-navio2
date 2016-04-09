1e5f6bba3179fc812464c30deb5cb54ad899abed
i-copter <icopter.productions@gmail.com>
2016-04-09 17:22:34 +0200

    Update variables to static

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -8,12 +8,12 @@
 #include <linux/watchdog.h>
 #include <unistd.h>
 
-static void _wdtOpen(int *fd);      // local file open wrapper
-static void _wdtClose(int *fd);     // local file close wrapper
+static void wdtDevOpen(int *fd);      // local file open wrapper
+static void wdtDevClose(int *fd);     // local file close wrapper
 
-int _deviceHandle;              // Device file handler
-int _watchdogTimeout = 15;
-bool _watchdogPreRun = false;   // First run, previously run
+static int deviceHandle;              // Device file handler
+static int watchdogTimeout = 15;
+static bool watchdogPreRun = false;   // First run, previously run
 
 
 #ifdef USERHOOK_INIT
@@ -66,41 +66,39 @@ void Copter::userhook_WatchDogLoop()
     // WatchDog Timer parameter Enabled
      if (g.wdt_enabled){
 
-         _wdtOpen(&_deviceHandle);
+         wdtDevOpen(&deviceHandle);
 
-         if (_watchdogPreRun == false){
-             ioctl(_deviceHandle, WDIOC_SETTIMEOUT, &_watchdogTimeout); //Set watchdot default timeout
-             _watchdogPreRun = true;            // Watchdog timer first run
+         if (watchdogPreRun == false){
+             ioctl(deviceHandle, WDIOC_SETTIMEOUT, &watchdogTimeout); //Set watchdot default timeout
+             watchdogPreRun = true;            // Watchdog timer first run
              }
 
-         if (ioctl(_deviceHandle, WDIOC_KEEPALIVE, 0) < 0)     // Reset the watchdog timer
+         if (ioctl(deviceHandle, WDIOC_KEEPALIVE, 0) < 0)     // Reset the watchdog timer
              hal.console->println("Failed to reset Watchdog Timer!");
 
-         _wdtClose(&_deviceHandle);
+         wdtDevClose(&deviceHandle);
 
-     }else if (_watchdogPreRun){         // Has the watchdog timer previously been enabled && WDT_ENABLED parameter been disabled
+     }else if (watchdogPreRun){         // Has the watchdog timer previously been enabled && WDT_ENABLED parameter been disabled
 
-         _wdtOpen(&_deviceHandle);
+         wdtDevOpen(&deviceHandle);
 
-         if (write(_deviceHandle,"V",1) < 0)       // Disable Watchdog timer
+         if (write(deviceHandle,"V",1) < 0)       // Disable Watchdog timer
              hal.console->println("Failed to stop watchdog Device");
 
-         _watchdogPreRun = false;                // Watchdog timer has been disabled so it has not been previously run
-         _wdtClose(&_deviceHandle);
+         watchdogPreRun = false;                // Watchdog timer has been disabled so it has not been previously run
+         wdtDevClose(&deviceHandle);
      }
  }
 #endif
 
- // Static file open, pass device handler as pointer
- static void _wdtOpen(int *fd)
+ static void wdtDevOpen(int *fd)
  {
      if ((*fd = open("/dev/watchdog",O_RDWR | O_NOCTTY)) < 0) //watchdog is automatically started when opened
          hal.console->println("Failed to open watchdog Device");
 
  }
 
- // Static file close, pass device handler as pointer
- static void _wdtClose(int *fd)
+ static void wdtDevClose(int *fd)
  {
      if (close(*fd) < 0)                // Close the handle
          hal.console->println("Failed to close watchdog device");
